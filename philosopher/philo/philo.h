7459c128da3159d1f849cd7768c3d220f860e5ae
Bilal Badda <bbadda@c1r6p8.1337.ma>
2024-09-07 12:58:53 +0100

    done

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: bbadda <bbadda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 12:16:13 by bbadda            #+#    #+#             */
-/*   Updated: 2024/09/03 13:36:43 by bbadda           ###   ########.fr       */
+/*   Updated: 2024/09/07 12:51:39 by bbadda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define PHILO_H
 
 # include <stdio.h>
-# include <string.h>
-# include <string.h>
 # include <stdlib.h>
 # include <pthread.h>
 # include <sys/time.h>
@@ -26,12 +24,15 @@ typedef struct s_philoinfo	t_philoinfo;
 
 typedef struct s_philo
 {
-	int			id;
-	int			ate;
-	size_t		should_die;
-	size_t		last_meal_time;
-	pthread_t	thread;
-	t_philoinfo	*info;
+	int				id;
+	int				ate;
+	size_t			should_die;
+	size_t			last_meal_time;
+	int				right_fork;
+	int				left_fork;
+	pthread_t		thread;
+	pthread_mutex_t	ate_m;
+	t_philoinfo		*info;
 }t_philo;
 
 typedef struct s_philoinfo
@@ -42,15 +43,18 @@ typedef struct s_philoinfo
 	long			time_to_sleep;
 	long			n_time_should_eat;
 	int				finished;
+	bool			is_die;
 	size_t			start_time;
-	int				eat;
+	int				all_ate;
 	t_philo			*philo;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print_m;
+	pthread_mutex_t	is_die_m;
 	pthread_mutex_t	should_die_m;
 	pthread_mutex_t	finished_m;
 	pthread_mutex_t	last_meal_m;
-	pthread_mutex_t	eat_m;
+	pthread_mutex_t	sleep_m;
+	pthread_mutex_t	all_ate_m;
 }t_philoinfo;
 
 //-----------------------check/create/init_things----------------------//
@@ -60,21 +64,22 @@ void			__creat_mutex(t_philoinfo *ph);
 void			__create_threads_and_philos(t_philoinfo *info);
 int				check_death_status(t_philo *each_philo);
 int				check_finished_status(t_philo *each_philo);
+int				check_death_and_hunger_status(t_philo *each_philo);
 size_t			check_last_meal_status(t_philo *each_philo);
-void			*__check_death(void *arg);
-void			*__check_hunger(void *arg);
+void			*__check_death_and_hunger(void *arg);
 //----------------------------utils_functions--------------------------//
 int				__strlen(const char *str);
 void			*__calloc(size_t count, size_t size);
 int				ft_atoi(const char *str);
-void			my_own_sleep(long time);
+int				cmp(const char *s1, const char *s2);
+void			my_own_sleep(t_philo *philo, long time);
 long			get_msec_time(void);
 // void			*__is_died(void *philo);
 long			get_msec_time(void);
 //---------------------------routine_and_print--------------------------//
 void			*ft_routine(void *philos);
 void			eating(t_philo *each_philo);
-void			sleep_and_then_think(t_philo *philo);
+void			sleep_time(t_philo *philo);
 void			__error(int index);
 int				__check_info(t_philoinfo info, int ac);
 void			mutex_print(t_philo *philo, char *message);
--- philosopher/philo/routine.c
+++ philosopher/philo/routine.c
@@ -6,69 +6,68 @@
 /*   By: bbadda <bbadda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 12:58:14 by bbadda            #+#    #+#             */
-/*   Updated: 2024/09/03 13:31:49 by bbadda           ###   ########.fr       */
+/*   Updated: 2024/09/07 12:52:19 by bbadda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	sleep_and_then_think(t_philo *philo)
+void	sleep_time(t_philo *philo)
 {
 	mutex_print(philo, "is sleeping");
-	my_own_sleep(philo->info->time_to_sleep);
-	mutex_print(philo, "is thinking");
+	my_own_sleep(philo, philo->info->time_to_sleep);
 }
 
 void	eating(t_philo *each_philo)
 {
 	mutex_print(each_philo, "is eating");
-	my_own_sleep(each_philo->info->time_to_eat);
 	pthread_mutex_lock(&each_philo->info->last_meal_m);
 	each_philo->last_meal_time = get_msec_time();
 	pthread_mutex_unlock(&each_philo->info->last_meal_m);
+	my_own_sleep(each_philo, each_philo->info->time_to_eat);
+	pthread_mutex_lock(&each_philo->ate_m);
 	each_philo->ate++;
-	if (each_philo->ate == each_philo->info->n_time_should_eat)
+	pthread_mutex_unlock(&each_philo->ate_m);
+	if (each_philo->ate == each_philo->info->n_time_should_eat 
+		&& each_philo->ate != -1)
 	{
-		pthread_mutex_lock(&each_philo->info->eat_m);
-		each_philo->info->eat++;
-		pthread_mutex_unlock(&each_philo->info->eat_m);
+		pthread_mutex_lock(&each_philo->info->all_ate_m);
+		each_philo->info->all_ate++;
+		pthread_mutex_unlock(&each_philo->info->all_ate_m);
+		each_philo->ate = -1;
 	}
 }
 
-static void	take_fork(t_philo *philo, int ph)
+static void	take_fork(t_philo *philo, int fork)
 {
-	pthread_mutex_lock(&philo->info->forks[ph]);
+	pthread_mutex_lock(&philo->info->forks[fork]);
 	mutex_print(philo, "has taken a fork");
 }
 
-static void	drop_fork(t_philo *philo, int ph1, int ph2)
+static void	drop_fork(t_philo *philo, int fork1, int fork2)
 {
-	pthread_mutex_unlock(&philo->info->forks[ph1]);
-	pthread_mutex_unlock(&philo->info->forks[ph2]);
+	pthread_mutex_unlock(&philo->info->forks[fork1]);
+	pthread_mutex_unlock(&philo->info->forks[fork2]);
 }
 
 void	*ft_routine(void *philo)
 {
 	t_philo		*each_philo;
-	int			right_fork;
-	int			left_fork;
 
 	each_philo = (t_philo *)philo;
-	while (!check_death_status(each_philo) && 
-		!check_finished_status(each_philo))
+	if (each_philo->id % 2 == 0)
 	{
-		right_fork = each_philo->id;
-		left_fork = (each_philo->id + 1) % each_philo->info->number_of_philos;
-		take_fork(each_philo, right_fork);
-		if (each_philo->info->number_of_philos == 1)
-		{
-			my_own_sleep(each_philo->info->time_to_die);
-			break ;
-		}
-		take_fork(each_philo, left_fork);
+		mutex_print(philo, "is sleeping");
+		my_own_sleep(philo, each_philo->info->time_to_sleep);
+	}
+	while (!check_death_and_hunger_status(each_philo))
+	{
+		mutex_print(philo, "is thinking");
+		take_fork(each_philo, each_philo->right_fork);
+		take_fork(each_philo, each_philo->left_fork);
 		eating(each_philo);
-		drop_fork(each_philo, right_fork, left_fork);
-		sleep_and_then_think(each_philo);
+		drop_fork(each_philo, each_philo->right_fork, each_philo->left_fork);
+		sleep_time(each_philo);
 	}
 	pthread_mutex_unlock(&each_philo->info->should_die_m);
 	pthread_mutex_unlock(&each_philo->info->finished_m);
